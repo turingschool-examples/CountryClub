3e377f09f3da94c8c021ca8b0e372aef003957bf
skylarbsandler <skylarbsandler@gmail.com>
2023-10-09 13:06:57 -0600

    finished all crud api calls

--- CountryClubAPI/Controllers/MembersController.cs
+++ CountryClubAPI/Controllers/MembersController.cs
@@ -1,6 +1,7 @@
 ﻿using CountryClubAPI.DataAccess;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CountryClubAPI.Models;
 
 namespace CountryClubAPI.Controllers
 {
@@ -15,10 +16,87 @@ namespace CountryClubAPI.Controllers
             _context = context;
         }
 
+        //GET: "/api/members"
+        [HttpGet]
         public IActionResult AllMembers()
         {
             var members = _context.Members;
 
+            return new JsonResult(members);
+        }
+
+        //Get: "/api/members/:id"
+        [HttpGet("{memberId}")]
+        public ActionResult GetMemberfromId(int memberId)
+        {
+            var member = _context.Members.Find(memberId);
+
+            if (member is null)
+            {
+                return NotFound();
+            }
+
+            Response.StatusCode = 200;
+
+            return new JsonResult(member);
+        }
+
+        //POST: "/api/members"
+        [HttpPost]
+        public ActionResult CreateMember(Member member)
+        {
+            if(!ModelState.IsValid)
+            {
+                Response.StatusCode = 400;
+                return BadRequest();
+            }
+            _context.Members.Add(member);
+            _context.SaveChanges();
+
+            Response.StatusCode = 201;
+            return new JsonResult(member);
+        }
+
+        [HttpPut("{memberId}")]
+        public ActionResult UpdateMember(int memberId, Member member)
+        {
+            var existingMember = _context.Members.Find(memberId);
+
+            if (!ModelState.IsValid)
+            {
+                Response.StatusCode = 400;
+            }
+
+            existingMember.LastName = member.LastName;
+            existingMember.FirstName = member.FirstName;
+            existingMember.Address = member.Address;
+            existingMember.ZipCode = member.ZipCode;
+            existingMember.Telephone = member.Telephone;
+            existingMember.RecommendedBy = member.RecommendedBy;
+            existingMember.JoinDate = member.JoinDate;
+
+            _context.Members.Update(existingMember);
+            _context.SaveChanges();
+
+            Response.StatusCode = 204;
+            return new JsonResult(existingMember); ;
+        }
+
+        [HttpDelete("{memberId}")]
+        public ActionResult DeleteMemberfromId(int memberId)
+        {
+            var members = _context.Members;
+            var member = members.Find(memberId);
+
+            if (member is null)
+            {
+                return NotFound();
+            }
+
+            _context.Members.Remove(member);
+            _context.SaveChanges();
+
+            Response.StatusCode = 204;
             return new JsonResult(members);
         }
     }
