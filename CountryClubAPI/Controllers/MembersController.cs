6fcdc0d1f402393d9be208bb3f86086785839810
iworsham <15iworsham@gmail.com>
2023-10-09 17:03:07 -0400

    changes

--- CountryClubAPI/Controllers/MembersController.cs
+++ CountryClubAPI/Controllers/MembersController.cs
@@ -1,6 +1,7 @@
 ﻿using CountryClubAPI.DataAccess;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CountryClubAPI.Models;
 
 namespace CountryClubAPI.Controllers
 {
@@ -27,6 +28,34 @@ namespace CountryClubAPI.Controllers
             var members = _context.Members.Find(id);
             
             return new JsonResult(members);
+        }
+        [HttpPost]
+        [Route("api/members")]
+        public IActionResult CreateMember(Member member)
+        {
+            _context.Members.Add(member);
+            _context.SaveChanges();
+
+            return new JsonResult(member);
+
+        }
+        [Route("api/members/edit/{id:int}")]
+        public IActionResult Edit(int id)
+        {
+            var members = _context.Members.Find(id);
+
+            return new JsonResult(members);
+        }
+
+        [HttpPost]
+        [Route("api/members/edit/{id:int}")]
+        public IActionResult Update(Member member)
+        {
+            
+            _context.Members.Update(member);
+            _context.SaveChanges();
+
+            return new JsonResult(member);
         }
     }
 }
